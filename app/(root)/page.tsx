fb1791beb24a9703ec1ed3088f16b507318ca49e
garcia718gt4 <58898368+garcia718gt4@users.noreply.github.com>
2023-11-17 18:57:18 -0800

    clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      This is a protected route
+      <UserButton afterSignOutUrl="/"/> 
+    </div>
+  );
+}
+ 
+export default SetupPage;
